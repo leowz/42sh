d3a5e4bbbe4ef2c8e09f274b5794e126d93d1537
JonathanGuillemot <guillemot.jonathan@gmail.com>
2026-04-22 09:47:04 +0200

    [builtin_cd] work in progress 0

---
+++ srcs/builtins/builtin_cd.c
@@ -0,0 +1,145 @@
+/**
+ * @file builtin_cd.c
+ * @brief Implementation of the cd builtin command for the 42sh shell.
+ * @author jguillem
+ */
+
+#include "42sh.h"
+#include "builtins.h"
+#include "limits.h"
+
+char	*readlink_malloc (char *filename)
+{
+	ssize_t	size;
+	ssize_t	bytes;
+	char	*buffer;
+
+	size = 100;
+	while (1)
+	{
+		buffer = malloc(sizeof(char) * size);
+		bytes = readlink (filename, buffer, size);
+		if (bytes < 0)
+			return (NULL);
+		if (bytes < size)
+			return (buffer);
+		free (buffer);
+		if (size < 16384)
+			size *= 2;
+		else
+			return (NULL);
+	}
+}
+
+static char	*join_paths(char *old_path, char *relative)
+{
+	char	*tmp;
+	char	*new_path;
+	size_t	old_path_len;
+
+	if (!old_path)
+		return (strdup(relative));
+
+	old_path_len = strlen(old_path);
+	if (old_path[old_path_len - 1] == '/')
+		tmp = strdup(old_path);
+	else
+	{
+		tmp = malloc(sizeof(char) * old_path_len + 2);
+		sprintf(tmp, "%s/", old_path);
+	}
+
+	new_path = malloc(sizeof(char) * (strlen(tmp) + strlen(relative) + 1));
+	sprintf(new_path, "%s%s", tmp, relative);
+	free(tmp);
+	return (new_path);
+}
+
+static int	change_directory(char *target, int physical)
+{
+	char	*path = NULL;
+	char	*directory;
+	char	*oldpwd;
+	char	*cwd;
+
+	directory = target;
+	if (!directory)
+		directory = getenv("HOME");
+	oldpwd = strdup(getenv("PWD") ? getenv("PWD") : "");
+	if (physical)
+		path = realpath(directory, NULL);
+	else
+	{
+		if (directory[0] == '/')
+			path = strdup(directory);
+		else
+			path = join_paths(oldpwd, directory);
+	}
+	if (chdir(path) < 0)
+	{
+		fprintf(stderr, "-42sh: cd: %s: No such file or directory\n", path);
+		free(path);
+		free(oldpwd);
+		return (1);
+	}
+	else
+	{
+		setenv("OLDPWD", oldpwd, 1);
+		if (physical)
+		{
+			cwd = getcwd(NULL, 0);
+			setenv("PWD", cwd, 1);
+			free(cwd);
+		}
+		else
+			setenv("PWD", path, 1);
+		free(path);
+		free(oldpwd);
+		return (0);
+	}
+}
+
+static int	detect_option(char *option)
+{
+	int	physical = 0;
+	int	i = 0;
+
+	while (option[++i])
+	{
+		if (option[i] == 'P')
+			physical = 1;
+		else if (option[i] == 'L')
+			physical = 0;
+		else
+		{
+			fprintf(stderr, "-42sh: cd: -%c: invalid option\n", option[i]);
+			fprintf(stderr, "cd: usage: cd [-L|[-P [-e]]] [-@] [dir]\n");
+			return (-1);
+		}
+	}
+	return (physical);
+}
+
+int	builtin_cd(struct s_shell *shell, int argc, char **argv)
+{
+	(void)shell;
+	(void)argc;
+	int	physical = 0;
+
+	++argv;
+	if (*argv && **argv == '-' && strlen(*argv) > 1)
+	{
+		if ((physical = detect_option(*argv)) == -1)
+			return (2);
+		argc--;
+		++argv;
+	}
+	if (argc == 1)
+		return (change_directory(NULL, physical));
+	if (argc > 2)
+	{
+		fprintf(stderr, "-42sh: cd: too many arguments\n");
+		return (2);
+	}
+	return (change_directory(*argv, physical));
+}
--- srcs/builtins/builtins.c
+++ srcs/builtins/builtins.c
@@ -21,6 +21,7 @@ typedef struct s_builtin_entry
 static const t_builtin_entry	g_builtins[] = {
 	{"history", builtin_history},
 	{"echo", builtin_echo},
+	{"cd", builtin_cd},
 	{NULL, NULL}
 };
 
