837bb76c9db117a344569feee5858a27021a9537
pulgamecanica <pulgamecanica11@gmail.com>
2026-05-27 14:06:40 +0200

    chore: handle signals when subshell in bg job

--- srcs/executor/exec_subshell.c
+++ srcs/executor/exec_subshell.c
@@ -112,6 +112,7 @@ static void	bg_child(t_shell *shell, t_ast *ast)
 			close(devnull);
 		}
 	}
+	shell->interactive = 0;
 	if (setup_redirections(ast->data.group->redirs, NULL) == -1)
 		_exit(1);
 	status = executor_execute(shell, ast->data.group->child);
